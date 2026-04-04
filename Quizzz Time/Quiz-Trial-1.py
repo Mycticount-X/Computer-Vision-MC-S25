20217cc097b3f52674edb1040419f9a20eab3f2f
Mycticount-X <mxlovers77@gmail.com>
2026-04-04 20:55:03 +0700

    Add: First Try of Quiz

---
+++ Quizzz Time/Quiz-Trial-1.py
@@ -0,0 +1,108 @@
+import numpy as np
+import os
+import cv2
+import matplotlib.pyplot as plt
+
+def Preprocessing(img, blurtype='gaussian', ksize=3):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.equalizeHist(img)
+
+    if blurtype == 'avg':
+        img = cv2.blur(img, (ksize, ksize))
+    elif blurtype == 'filter2d':
+        kernel = np.ones((ksize, ksize), np.float32) / (ksize / ksize)
+        img = cv2.filter2D(img, -1, kernel)
+    elif blurtype == 'median':
+        img = cv2.medianBlur(img, ksize)
+    elif blurtype == 'gaussian':
+        img = cv2.GaussianBlur(img, (ksize, ksize), 0)
+    elif blurtype == 'bilateral':
+        img = cv2.bilateralFilter(img, ksize, 75, 75)
+    else:
+        img = cv2.GaussianBlur(img, (ksize, ksize), 0)
+    
+    return img
+
+def Indentify(img_path, dataset_path, algo='AKAZE', blurtype='gaussian', ksize=3):
+    img = cv2.imread(img_path)
+    if img is None:
+        print('File tidak ditemukan')
+        return None
+    
+    img = Preprocessing(img)
+
+    # Desc Algo
+    if algo == 'AKAZE':
+        descriptor = cv2.AKAZE.create()
+    elif algo == 'SIFT':
+        descriptor = cv2.SIFT.create()
+    elif algo == 'ORB':
+        descriptor = cv2.ORB.create()
+    else:
+        print('Algoritma tidak dikenali')
+        print('Menggunakan Akaze sebagai pengganti')
+        descriptor = cv2.AKAZE.create()
+    
+    # Keypoint & Desc Extraction
+    kp_targ, desc_targ = descriptor.detectAndCompute(img, None)
+
+    if algo == 'ORB':
+        matcher = cv2.BFMatcher(cv2.NORM_HAMMING, False)
+    else:
+        desc_targ = np.float32(desc_targ)
+        
+        idx_params = dict(algorithm=1, trees=5)
+        src_params = dict(checks=50)
+        matcher = cv2.FlannBasedMatcher(idx_params, src_params)
+    
+    # Best Match
+    best_match = 0
+    best_data = {}
+
+    for filename in os.listdir(dataset_path):
+        if not filename.lower().endswith(('.jpg', '.png', '.jpeg')):
+            continue
+
+        ref_path = os.path.join(dataset_path, filename)
+        ref_img = cv2.imread(ref_path)
+        ref_img = Preprocessing(ref_img)
+
+        # Keypoint & Desc Extraction
+        kp_ref, desc_ref = descriptor.detectAndCompute(ref_img, None)
+
+        if desc_ref is None or len(desc_ref) < 2:
+            continue
+
+        if algo != 'ORB':
+            desc_ref = np.float32(desc_ref)
+        
+        # Lowe's Ratio Test
+        matches = matcher.knnMatch(desc_targ, desc_ref)
+        good_matches = []
+
+        for m, n in matches:
+            if m.distance < 0.7 * n.distance:
+                good_matches.append(m)
+        
+        # Best Match
+        if len(good_matches) > best_match:
+            best_match = good_matches
+            best_data = {
+                'name': filename,
+                'image': ref_img,
+                'keypoints': kp_ref,
+                'matches': good_matches
+            }
+    
+    if best_match > 0:
+        print('PoveKamon Terdeteksi!!')
+        print(f'[{algo} | {blurtype.upper()}]')
+        print('')
+        print('Best Match:')
+        print(f'{best_data["name"]} ({best_data['matches']*100}%)')
+        print('')
+
+
+
+    
+    
