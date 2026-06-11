c81d37fa4ca5512890122dd0329490d31bd0ae15
Mycticount-X <mxlovers77@gmail.com>
2026-06-11 10:53:37 +0700

    Siap Membantai UAP... Siap kan?

--- ngUAP Time/UAP-Trial-3.py
+++ ngUAP Time/UAP-Trial-3.py
@@ -1,63 +1,54 @@
-import math
-import cv2
 import os
+import cv2
+import math
 import numpy as np
 
-TRAIN_PATH = './Dataset/Train/'
-TEST_PATH = './Dataset/Test/'
-MODEL_PATH = './model_recognizer.xml'
 CASCADE_PATH = './haarcascade_frontalface_default.xml'
+MODEL_PATH = './model_recognizer.xml'
+TRAIN_PATH = './Dataset/train/'
+TEST_PATH = './Dataset/test/'
 
 def TrainTest ():
-    print('Initialize Training...\n')
-
-    # Init
-    cascaderx = cv2.CascadeClassifier(CASCADE_PATH)
+    # Train
+    classifierx = cv2.CascadeClassifier(CASCADE_PATH)
     recognizerx = cv2.face.LBPHFaceRecognizer_create()
     
     face_list = []
     class_list = []
     classes = os.listdir(TRAIN_PATH)
 
-    # Setup Rect
     for idx, clx in enumerate(classes):
         folder_path = os.path.join(TRAIN_PATH, clx)
-
+        
         for raw_path in os.listdir(folder_path):
             img_path = os.path.join(folder_path, raw_path)
-            img = cv2.imread(img_path)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
-            detface = cascaderx.detectMultiScale(img, 1.2, 5)
+            detface = classifierx.detectMultiScale(img)
             if len(detface) < 1: continue
 
             for x, y, w, h in detface:
-                imgface = img[y:y+h, x:x+w]
+                imgface = img[y:y+h, x:x+h]
                 face_list.append(imgface)
                 class_list.append(idx)
-
-    # Train
+    
     recognizerx.train(face_list, np.array(class_list))
     recognizerx.save(MODEL_PATH)
-    print('Model Trained & Saved\n')
 
-    # Init
-    print('Initialize Testing...\n')
+    # Test
     total_images = 0
     total_correct = 0
 
-    # Setup Rect & Test
     for true_name in os.listdir(TEST_PATH):
         if true_name not in classes: continue
         true_id = classes.index(true_name)
         folder_path = os.path.join(TEST_PATH, true_name)
-
+        
         for raw_path in os.listdir(folder_path):
             img_path = os.path.join(folder_path, raw_path)
-            img = cv2.imread(img_path)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
-            detface = cascaderx.detectMultiScale(img, 1.2, 5)
+            detface = classifierx.detectMultiScale(img)
             if len(detface) < 1: continue
 
             for x, y, w, h in detface:
@@ -67,66 +58,65 @@ def TrainTest ():
                 if res_id == true_id:
                     total_correct += 1
                 total_images += 1
-
+        
     # Accuracy
     if total_images > 0:
         acc = (total_correct / total_images) * 100
-        print('Model Tested')
+
+        print('Model Trained')
         print(f'Accuracy: {acc:.2f}%')
     else:
-        print('No Face Image Found or Detected to Test')
-    print('')
-
+        print('(!) No Face Images Found or Detected in Test Dataset')
+    
     return classes
 
 def Predict (classes):
-    # Init & Check
     if not os.path.exists(MODEL_PATH):
-        print('(!) Cannot Find Model! Please Train the Model First')
+        print('(!) Model not Found! Please Train the Model First!')
         return None
-
-    cascaderx = cv2.CascadeClassifier(CASCADE_PATH)
+    
+    classifierx = cv2.CascadeClassifier(CASCADE_PATH)
     recognizerx = cv2.face.LBPHFaceRecognizer_create()
     recognizerx.read(MODEL_PATH)
 
     # Input
-    img_path = input('Please Input the Target Image Path (Absolute Path): ')
+    img_path = input('Please Input Target Image Path (Absolute Path): ')
     if not os.path.exists(img_path):
         print('(!) Invalid Path')
         return None
-    
+
     img_ori = cv2.imread(img_path)
     img = cv2.cvtColor(img_ori, cv2.COLOR_BGR2GRAY)
 
-    # Detect Face
-    detface = cascaderx.detectMultiScale(img, 1.2, 5)
-
+    # Detect
+    detface = classifierx.detectMultiScale(img)
     if len(detface) < 1:
         print('(!) No Face Detected')
         return None
 
-    # Draw Rect
+    # Rect
     for x, y, w, h in detface:
-        imgface = img[y:y+h,x:x+w]
+        imgface = img[y:y+h, x:x+w]
         res_id, confidence = recognizerx.predict(imgface)
         confidence = math.floor(confidence * 100) / 100
 
         print(f'Detected Face: {classes[res_id]}')
         print(f'Confidence Level: {confidence}')
 
-        cv2.rectangle(img_ori, (x,y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.rectangle(img_ori, (x,y), (x+w,y+h), (0, 255, 0), 2)
         text = f'{classes[res_id]}: {confidence}'
         cv2.putText(img_ori, text, (x,y-10), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
 
+
     # Show
-    cv2.imshow("Result", img_ori)
+    cv2.imshow('Result', img_ori)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     classes = os.listdir(TRAIN_PATH) if os.path.exists(TRAIN_PATH) else None
     if classes is None:
-        print('(!) Warning! Dataset Folder Not Found')
+        print('(!) Warning! Train Dataset Not Found!')
     
     while True:
         print('')
@@ -142,7 +132,8 @@ if __name__ == '__main__':
         elif cc == '2':
             Predict(classes)
         elif cc == '3':
-            print('OK, Byee')
+            print('Alright, Have a Great Day!')
             break
         else:
             print('(!) Invalid Input')
+    
